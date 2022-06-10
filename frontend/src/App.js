01fff173aa839e83bcdf9262c362ea3f4aa4fc4d
arabiran-prans <96466078+arabiran-prans@users.noreply.github.com>
2022-06-10 15:27:54 +0800

    data-pics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">NorthSide</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Feature Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
