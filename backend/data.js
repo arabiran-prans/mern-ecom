07b244698f2fdb30740772272c1f2f9b11962dc1
arabiran-prans <96466078+arabiran-prans@users.noreply.github.com>
2022-07-18 10:30:40 +0800

    Seed Sample Users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'baser amer',
+      email: 'admin@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'woker',
+      email: 'wokersagedli@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       //_id: '1',
